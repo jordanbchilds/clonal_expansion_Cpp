15a74da9842da4368b43e3e8e4e179d3f0916ead
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-08-30 09:57:34 +0100

    Update gillespie_codelet.cpp

--- Gillespie/ipu/gillespie_codelet.cpp
+++ Gillespie/ipu/gillespie_codelet.cpp
@@ -237,7 +237,7 @@ public:
 		int* output_ptr = &output[0][0];
 
 		//gillespied(x_init, react_rates, con_rates, output_ptr, spn);
-		*out = S_mat[1][1];
+		*out = S_mat[2][0];
 		return true;
 	}
 };
