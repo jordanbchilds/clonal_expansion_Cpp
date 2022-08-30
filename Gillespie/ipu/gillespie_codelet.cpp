83ca12eab9f1fd6b2655a565f5ce7de05999de60
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-08-30 11:58:02 +0100

    Update gillespie_codelet.cpp

--- Gillespie/ipu/gillespie_codelet.cpp
+++ Gillespie/ipu/gillespie_codelet.cpp
@@ -180,19 +180,22 @@ public:
 			else
 			 tt += rand_exp(haz_total);
 			
+			/*
 			if( tt>=target ){
-				/*
+				
 				for(int j=0; j<n_species; ++j){
 					*(out_array+count*n_species+j ) = x[j];
 				}
-				*/
-				
 				w_popDyn[count] = x[0];
 				m_popDyn[count] = x[1];
 				count += 1;
 				target += step_out;
 			}
+			*/
 			
+			w_popDyn[count] = x[0];
+			m_popDyn[count] = x[1];
+			count += 1;
 			
 			int r = rand_react(hazards);
 			
