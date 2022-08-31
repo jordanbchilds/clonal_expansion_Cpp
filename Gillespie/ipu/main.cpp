ed233a5fce3fe50935a1860bd1b8ee7fb7ad084b
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-08-31 14:32:33 +0100

    Update main.cpp

--- Gillespie/ipu/main.cpp
+++ Gillespie/ipu/main.cpp
@@ -185,7 +185,7 @@ int main()
 	std::cout << "Rate=" << (float(datasetSize)/elapsed_seconds.count()) << std::endl;
 	
 	for(size_t i=0; i<datasetSize; ++i){
-		std::cout<< (int) output[i] <<std::endl;
+		std::cout<< output[i] <<std::endl;
 	}
 	
 	/*
