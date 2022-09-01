b64a029a4f02c6c26fc5f3ca3bde2e18e8f2ce35
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-01 15:55:00 +0100

    timeout increase did not work...

--- Gillespie/ipu/main.cpp
+++ Gillespie/ipu/main.cpp
@@ -162,9 +162,6 @@ int main()
 	// prog.add(PrintTensor("output", output));
 	// Create the engine
 	Engine engine(graph, prog);
-	
-	engine.hostSyncTimeout = 700;
-	
 	engine.load(device);
 
 	auto start = std::chrono::system_clock::now();
