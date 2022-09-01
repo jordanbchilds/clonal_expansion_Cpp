99d4b67367126ea745c57c3d4a2dd3e1f4c1e88e
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-01 14:35:52 +0100

    Output trials

--- Gillespie/ipu/gillespie_codelet.cpp
+++ Gillespie/ipu/gillespie_codelet.cpp
@@ -196,9 +196,11 @@ public:
 		
 		gillespied(x_init, react_rates, con_rates, output_ptr, spn);
 		
-		for(int i=0; i<spn.Nout; i+=2){
-			out[i] = output[i/2][0];
-			out[i+1] = output[i/2][1];
+		int index = 0;
+		for(int i=0; i<spn.Nout; ++1){
+			out[index] = output[i][0];
+			out[index+1] = output[i][1];
+			index += 2;
 		}
 		
 		return true;
