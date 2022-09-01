9c69563c6c7e24e5a4d5ad4ff3fb6c664f7211f3
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-01 19:16:58 +0100

    Update main.cpp

--- Gillespie/ipu/main.cpp
+++ Gillespie/ipu/main.cpp
@@ -96,7 +96,7 @@ int main()
 	std::normal_distribution<float> rate_dist(3.06e-8,5e-9);
 	std::normal_distribution<float> con_dist(2e03, 5e-4);
 	std::uniform_real_distribution<float> ML_dist(0.45,0.55);
-	std::normal_distributionL<float> CN_dist(1000, 100);
+	std::normal_distribution<float> CN_dist(1000, 100);
 	
     for (int i = 0; i < datasetSize; ++i){
 		//w_initVals[i] = w_initBoss;
