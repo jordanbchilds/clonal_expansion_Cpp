0f467c721ece0adc74d91420a4a725a4a61aee89
Conor Lawless <cnr.lwlss@gmail.com>
2022-09-02 14:38:40 +0100

    Moar plots

--- Gillespie/ipu/bigPlot.R
+++ Gillespie/ipu/bigPlot.R
@@ -22,3 +22,19 @@ png("CopyNumber.png",type="cairo-png",width=480*5,height=480*5,pointsize=12*5)
 plot(NULL,xlim=range(t),ylim=c(0.0,max(cn)),xlab="Time (y)",ylab="Total Copy Number")
 for(i in 1:Nmax) points(t,cn[i,],type="l",col=rgb(0,0,0,0.01))
 dev.off()
+
+Nbr = 750
+br = seq(0,Nbr)/Nbr
+mids = (br[2:length(br)]+br[1:(length(br)-1)])/2
+res = matrix(nrow=length(t),ncol=length(mids))
+for(i in 1:length(t)){
+ res[i,] = hist(mutload[,i], br, plot = FALSE)$density
+}
+png("Rainbow.png",width=2000,height=2000)
+ image(res[5:121,],col = rainbow(10000))
+dev.off()
+pdf("Densities.pdf")
+for(i in 1:length(t)){
+plot(mids,res[i,],type="l",main=paste("t =",t[i]),xlab="Mutation load",ylab="Density")
+}
+dev.off()
