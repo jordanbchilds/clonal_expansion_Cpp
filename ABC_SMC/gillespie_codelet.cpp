625322164822d0387cf76bacb2445ead6fc95347
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-07 14:52:35 +0100

    Update gillespie_codelet.cpp

--- ABC_SMC/gillespie_codelet.cpp
+++ ABC_SMC/gillespie_codelet.cpp
@@ -123,7 +123,7 @@ public:
 
 			tt += rand_exp(haz_total);
 
-			if( tt >= *(times+count) && count<=nTimes ){
+			while( tt >= *(times+count) && count<=nTimes ){
 				*(out_array+count*n_species) = x[0];
 				*(out_array+count*n_species+1) = x[1];
 				count += 1;
