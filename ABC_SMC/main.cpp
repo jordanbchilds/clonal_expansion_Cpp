3bd84764cf36be029f37e4bbfcf54eb9910676dc
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-07 11:17:31 +0100

    Trying to stream a scalar.

--- ABC_SMC/main.cpp
+++ ABC_SMC/main.cpp
@@ -151,7 +151,7 @@ std::vector<Program> buildGraphAndPrograms( poplar::Graph &graph, long unsigned 
 
 	// SHOULD WE PRE-COMPILE GILLESPIED? HOW YOU DO THAT?
 	graph.addCodelets("gillespie_codelet.cpp");
-	Tensor Nout = graph.addVariable(INT, {2});
+	Tensor Nout = graph.addVariable(INT, {1});
 	Tensor times = graph.addVariable(FLOAT, {nTimes}, "data_times");
 	Tensor theta = graph.addVariable(FLOAT, {nParam}, "model_params");
 	Tensor output = graph.addVariable(INT, {datasetSize, nTimes*2}, "output");
@@ -180,7 +180,7 @@ std::vector<Program> buildGraphAndPrograms( poplar::Graph &graph, long unsigned 
 	graph.createHostRead("output-read", output);
 	
 	// Create streams that allow reading and writing of the variables:
-    auto nTimes_stream = graph.addHostToDeviceFIFO("write_nTimes", INT, 2);
+    auto nTimes_stream = graph.addHostToDeviceFIFO("write_nTimes", INT, 1);
     auto times_stream = graph.addHostToDeviceFIFO("write_dataTimes", FLOAT, nTimes);
 	auto param_stream = graph.addHostToDeviceFIFO("write_theta", FLOAT, nParam);
 	
@@ -194,7 +194,7 @@ std::vector<Program> buildGraphAndPrograms( poplar::Graph &graph, long unsigned 
 void executeGraphProgram(float* theta_ptr, int nParam, float* outTimes_ptr, long unsigned int* nTimes_ptr, poplar::Engine &engine) { // poplar::Device &device, std::vector<Program> progs, poplar::Graph &graph,
 	
 	int n = *nTimes_ptr;
-	engine.connectStream("write_nTimes", nTimes_ptr, nTimes_ptr+1);
+	engine.connectStream("write_nTimes", nTimes_ptr, nTimes_ptr);
 	engine.connectStream("write_dataTimes", outTimes_ptr, outTimes_ptr+*nTimes_ptr);
 	engine.connectStream("write_theta", theta_ptr, theta_ptr+nParam);
 	
@@ -290,7 +290,6 @@ int main() {
 	std::normal_distribution<float> CN_dist(1e3, 100);
 
 	float times[nTimes] = {25.0,55.0,65.0};
-	long unsigned int nTimes_vec[2] = {nTimes, nTimes};
 	float Tmax = 120.0*365.0 ;
 	float theta[nParam] ;
 	float* theta_ptr = &theta[0];
@@ -329,7 +328,7 @@ int main() {
 	for(int i=0; i<Ntheta; ++i){
 		for(int k=0; k<nParam; ++k){ *(theta_ptr+k) = param_space[i][k]; }
 				
-		executeGraphProgram(theta_ptr, nParam, &times[0], &nTimes_vec[0], engine);
+		executeGraphProgram(theta_ptr, nParam, &times[0], &nTimes, engine);
 		std::vector<int> cpu_vector( totalThreads * nTimes * 2 );
 		engine.readTensor("output-read", cpu_vector.data(), cpu_vector.data()+cpu_vector.size());
 
