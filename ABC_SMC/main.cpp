82f8cb1493fcacc001b8ee9a33caad5dfe61b64f
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-08 21:48:49 +0100

    Update main.cpp

--- ABC_SMC/main.cpp
+++ ABC_SMC/main.cpp
@@ -333,9 +333,14 @@ int main() {
 			sim_summ[0][t][1] = myStdDev(mutation_load[t], totalThreads, sim_summ[0][t][0]);
 			sim_summ[1][t][0] = myMean(copy_number[t], totalThreads);
 			sim_summ[1][t][1] = myStdDev(copy_number[t], totalThreads, sim_summ[1][t][0]);
+			
+			cout << sim_summ[0][t][0] << " " ;
+			cout << sim_summ[0][t][1] << endl;
+			cout << sim_summ[1][t][0] << " ";
+			cout << sim_summ[1][t][1] << endl;
 		}
-		double d = squared_dist(&sim_summ[0][0][0], &data_summ[0][0][0], nTimes, 2);
-		cout<< d << endl;
+		// double d = squared_dist(&sim_summ[0][0][0], &data_summ[0][0][0], nTimes, 2);
+		// cout<< d << endl;
 	}
 	return 0;
 }
