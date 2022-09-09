5a3f84a48263c66e3de29b1640a118448c680d19
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-09 13:57:28 +0100

    Update main.cpp

--- time_gillespie/no_control/main.cpp
+++ time_gillespie/no_control/main.cpp
@@ -130,7 +130,7 @@ int main() {
 		
 		chrono::duration<double, std::milli> ms_double = start - end;
 		
-		simTimes = ms_double.count();
+		simTimes[t] = ms_double.count() ;
 		cout<< ms_double.count() << endl;
 	}
 	
