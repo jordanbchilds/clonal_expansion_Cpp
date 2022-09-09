62f6f01a55591f49f059945882c5e649218c1b2b
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-09-09 17:17:53 +0100

    Update main.cpp

--- ABC_SMC/main.cpp
+++ ABC_SMC/main.cpp
@@ -322,7 +322,7 @@ int main() {
 	float theta[nParam]  = {500.0, 500.0, 2.64e-3, 2.64e-3, 2.64e-3, 2.64e-3, 0.0, 2e-3, 2e-3};
 	float* theta_ptr = &theta[0];
 	float* times_ptr = &times[0];
-	int Ntheta = 1;
+	int Ntheta = 10;
 
 	for(int i=0; i<Ntheta; ++i){
 		
@@ -351,31 +351,35 @@ int main() {
 					index += 1;
 				}
 			}
+			index -= 1;
+			
 			double ml_sum = 0.0;
 			double cn_sum = 0.0;
-			for(int kk=0; kk<(index-1); ++kk){
+			for(int kk=0; kk<index; ++kk){
 				ml_sum += mutation_load[t][kk];
 				cn_sum += copy_number[t][kk];
 			}
 			double ml_mean = ml_sum/float(index);
 			double cn_mean = cn_sum/float(index);
 			
-			sim_summ[0][t][0] = myMean(&mutation_load[t][0], index-1);
-			sim_summ[1][t][0] = myMean(&copy_number[t][0], index-1);
+			sim_summ[0][t][0] = myMean(&mutation_load[t][0], index);
+			sim_summ[1][t][0] = myMean(&copy_number[t][0], index);
 			double ml_sqDiff = 0.0;
 			double cn_sqDiff = 0.0;
-			for(int kk=0; kk<(index-1); ++kk){
+			for(int kk=0; kk<index; ++kk){
 				ml_sqDiff += (mutation_load[t][kk] - ml_mean)*(mutation_load[t][kk] - ml_mean) ;
 				cn_sqDiff += (copy_number[t][kk] - cn_mean)*(copy_number[t][kk] - cn_mean) ;
 			}
-			sim_summ[0][t][1] = ml_sqDiff / float(index-2) ;
-			sim_summ[1][t][1] = cn_sqDiff / float(index-2) ;
+			sim_summ[0][t][1] = ml_sqDiff / float(index-1) ;
+			sim_summ[1][t][1] = cn_sqDiff / float(index-1) ;
 			
 			cout << sim_summ[0][t][0] << " " << sim_summ[0][t][1] << endl;
 			cout << sim_summ[1][t][0] << " " << sim_summ[1][t][1] << endl;
 
 		}
 		double d = squared_dist(&sim_summ[0][0][0], &data_summ[0][0][0], nTimes, 2);
+		d += squared_dist&sim_summ[1][0][0], &data_summ[1][0][0], nTimes, 2);
+		
 		cout << "Distance: " << " " << d << endl;
 	}
 	return 0;
