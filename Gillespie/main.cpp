d419d056c136fa720a47472e58c9abf49c7f5f79
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2022-11-11 16:10:05 +0000

    Gillespie/main multi-threading sims

--- Gillespie/main.cpp
+++ Gillespie/main.cpp
@@ -90,7 +90,7 @@ void executeGraphProgram(float* theta_ptr, long unsigned int nParam, float* time
 int main() {
 	const int numberOfCores = 4; // depends on the POD avaible (POD4 = <=4 cores)
 	const int numberOfTiles = 1472; //  <= 1472
-	const int threadsPerTile = 1; // <= 6
+	const int threadsPerTile = 6; // <= 6
 	
 	long unsigned int totalThreads = numberOfCores * numberOfTiles * threadsPerTile ;
 	
